e6ad2bc90fc37a5f3569f904e9ab68a9e1839f64
Harold <hlgoco.hg@gmail.com>
2024-11-14 22:44:28 +0800

    Bash exercise usign variables

---
+++ bash_tutorial/exercise_2_compute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+price=15
+echo "Today's price is \$$price"
+echo "Tomorrow's price is \$$((price*2))"
---
+++ bash_tutorial/knock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo knock, knock
+echo "Who's there?"
+echo "It's me, $USER"
